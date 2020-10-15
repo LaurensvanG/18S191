311ceb71d0e0d713bd06f9a0aa72cbde3552874d
LaurensvanG <57599515+LaurensvanG@users.noreply.github.com>
2020-10-15 23:57:21 +0200

    More robust webcam call
    On my computer (Chrome & Firefox/Windows 10) the webcam would not start and the console gave an error (posted at the end of this pull request), because `this.currentScript.parentElement` does not seem to be a call that can be depended on. 
    Giving the `span` a new unique class and looping over that class (with `document.querySelectorAll` and `forEach`), the webcam seems to work reliably.
    
    Error from Chrome console:
    `TypeError: Cannot read property 'currentScript' of undefined
        at fn (eval at execute_dynamic_function (CellOutput.js:150), <anonymous>:8:22)
        at eval (eval at execute_dynamic_function (CellOutput.js:150), <anonymous>:105:16)
        at execute_dynamic_function (CellOutput.js:150)
        at execute_scripttags (CellOutput.js:181)
        at CellOutput.js:229
        at run (CellOutput.js:208)
        at Object.__ (CellOutput.js:228)
        at ee (standalone.module.js:1)
        at standalone.module.js:1
        at Array.filter (<anonymous>)`

--- homework/homework1/hw1.jl
+++ homework/homework1/hw1.jl
@@ -1094,88 +1094,88 @@ bigbreak
 # ╔═╡ dfb7c6be-ee0d-11ea-194e-9758857f7b20
 function camera_input(;max_size=200, default_url="https://i.imgur.com/SUmi94P.png")
 """
-<span class="pl-image waiting-for-permission">
-<style>
-	
-	.pl-image.popped-out {
-		position: fixed;
-		top: 0;
-		right: 0;
-		z-index: 5;
-	}
+<span class="pl-image waiting-for-permission webcam-span">
+	<style>
 
-	.pl-image #video-container {
-		width: 250px;
-	}
-
-	.pl-image video {
-		border-radius: 1rem 1rem 0 0;
-	}
-	.pl-image.waiting-for-permission #video-container {
-		display: none;
-	}
-	.pl-image #prompt {
-		display: none;
-	}
-	.pl-image.waiting-for-permission #prompt {
-		width: 250px;
-		height: 200px;
-		display: grid;
-		place-items: center;
-		font-family: monospace;
-		font-weight: bold;
-		text-decoration: underline;
-		cursor: pointer;
-		border: 5px dashed rgba(0,0,0,.5);
-	}
-
-	.pl-image video {
-		display: block;
-	}
-	.pl-image .bar {
-		width: inherit;
-		display: flex;
-		z-index: 6;
-	}
-	.pl-image .bar#top {
-		position: absolute;
-		flex-direction: column;
-	}
-	
-	.pl-image .bar#bottom {
-		background: black;
-		border-radius: 0 0 1rem 1rem;
-	}
-	.pl-image .bar button {
-		flex: 0 0 auto;
-		background: rgba(255,255,255,.8);
-		border: none;
-		width: 2rem;
-		height: 2rem;
-		border-radius: 100%;
-		cursor: pointer;
-		z-index: 7;
-	}
-	.pl-image .bar button#shutter {
-		width: 3rem;
-		height: 3rem;
-		margin: -1.5rem auto .2rem auto;
-	}
-
-	.pl-image video.takepicture {
-		animation: pictureflash 200ms linear;
-	}
-
-	@keyframes pictureflash {
-		0% {
-			filter: grayscale(1.0) contrast(2.0);
+		.pl-image.popped-out {
+			position: fixed;
+			top: 0;
+			right: 0;
+			z-index: 5;
 		}
 
-		100% {
-			filter: grayscale(0.0) contrast(1.0);
+		.pl-image #video-container {
+			width: 250px;
 		}
-	}
-</style>
+
+		.pl-image video {
+			border-radius: 1rem 1rem 0 0;
+		}
+		.pl-image.waiting-for-permission #video-container {
+			display: none;
+		}
+		.pl-image #prompt {
+			display: none;
+		}
+		.pl-image.waiting-for-permission #prompt {
+			width: 250px;
+			height: 200px;
+			display: grid;
+			place-items: center;
+			font-family: monospace;
+			font-weight: bold;
+			text-decoration: underline;
+			cursor: pointer;
+			border: 5px dashed rgba(0,0,0,.5);
+		}
+
+		.pl-image video {
+			display: block;
+		}
+		.pl-image .bar {
+			width: inherit;
+			display: flex;
+			z-index: 6;
+		}
+		.pl-image .bar#top {
+			position: absolute;
+			flex-direction: column;
+		}
+
+		.pl-image .bar#bottom {
+			background: black;
+			border-radius: 0 0 1rem 1rem;
+		}
+		.pl-image .bar button {
+			flex: 0 0 auto;
+			background: rgba(255,255,255,.8);
+			border: none;
+			width: 2rem;
+			height: 2rem;
+			border-radius: 100%;
+			cursor: pointer;
+			z-index: 7;
+		}
+		.pl-image .bar button#shutter {
+			width: 3rem;
+			height: 3rem;
+			margin: -1.5rem auto .2rem auto;
+		}
+
+		.pl-image video.takepicture {
+			animation: pictureflash 200ms linear;
+		}
+
+		@keyframes pictureflash {
+			0% {
+				filter: grayscale(1.0) contrast(2.0);
+			}
+
+			100% {
+				filter: grayscale(0.0) contrast(1.0);
+			}
+		}
+	</style>
 
 	<div id="video-container">
 		<div id="top" class="bar">
@@ -1194,103 +1194,104 @@ function camera_input(;max_size=200, default_url="https://i.imgur.com/SUmi94P.pn
 		</span>
 	</div>
 
-<script>
-	// based on https://github.com/fonsp/printi-static (by the same author)
+	<script>
+		// based on https://github.com/fonsp/printi-static (by the same author)
 
-	const span = this.currentScript.parentElement
-	const video = span.querySelector("video")
-	const popout = span.querySelector("button#pop-out")
-	const stop = span.querySelector("button#stop")
-	const shutter = span.querySelector("button#shutter")
-	const prompt = span.querySelector(".pl-image #prompt")
-
-	const maxsize = $(max_size)
-
-	const send_source = (source, src_width, src_height) => {
-		const scale = Math.min(1.0, maxsize / src_width, maxsize / src_height)
-
-		const width = Math.floor(src_width * scale)
-		const height = Math.floor(src_height * scale)
-
-		const canvas = html`<canvas width=\${width} height=\${height}>`
-		const ctx = canvas.getContext("2d")
-		ctx.drawImage(source, 0, 0, width, height)
-
-		span.value = {
-			width: width,
-			height: height,
-			data: ctx.getImageData(0, 0, width, height).data,
-		}
-		span.dispatchEvent(new CustomEvent("input"))
-	}
+		document.querySelectorAll(".webcam-span").forEach(span => {
+			const video = span.querySelector("video")
+			const popout = span.querySelector("button#pop-out")
+			const stop = span.querySelector("button#stop")
+			const shutter = span.querySelector("button#shutter")
+			const prompt = span.querySelector(".pl-image #prompt")
 	
-	const clear_camera = () => {
-		window.stream.getTracks().forEach(s => s.stop());
-		video.srcObject = null;
-
-		span.classList.add("waiting-for-permission");
-	}
-
-	prompt.onclick = () => {
-		navigator.mediaDevices.getUserMedia({
-			audio: false,
-			video: {
-				facingMode: "environment",
-			},
-		}).then(function(stream) {
-
-			stream.onend = console.log
-
-			window.stream = stream
-			video.srcObject = stream
-			window.cameraConnected = true
-			video.controls = false
-			video.play()
-			video.controls = false
-
-			span.classList.remove("waiting-for-permission");
-
-		}).catch(function(error) {
-			console.log(error)
+			const maxsize = $(max_size)
+	
+			const send_source = (source, src_width, src_height) => {
+				const scale = Math.min(1.0, maxsize / src_width, maxsize / src_height)
+	
+				const width = Math.floor(src_width * scale)
+				const height = Math.floor(src_height * scale)
+	
+				const canvas = html`<canvas width=\${width} height=\${height}>`
+				const ctx = canvas.getContext("2d")
+				ctx.drawImage(source, 0, 0, width, height)
+	
+				span.value = {
+					width: width,
+					height: height,
+					data: ctx.getImageData(0, 0, width, height).data,
+				}
+				span.dispatchEvent(new CustomEvent("input"))
+			}
+			
+			const clear_camera = () => {
+				window.stream.getTracks().forEach(s => s.stop());
+				video.srcObject = null;
+	
+				span.classList.add("waiting-for-permission");
+			}
+	
+			prompt.onclick = () => {
+				navigator.mediaDevices.getUserMedia({
+					audio: false,
+					video: {
+						facingMode: "environment",
+					},
+				}).then(function(stream) {
+	
+					stream.onend = console.log
+	
+					window.stream = stream
+					video.srcObject = stream
+					window.cameraConnected = true
+					video.controls = false
+					video.play()
+					video.controls = false
+	
+					span.classList.remove("waiting-for-permission");
+	
+				}).catch(function(error) {
+					console.log(error)
+				});
+			}
+			stop.onclick = () => {
+				clear_camera()
+			}
+			popout.onclick = () => {
+				span.classList.toggle("popped-out")
+			}
+	
+			shutter.onclick = () => {
+				const cl = video.classList
+				cl.remove("takepicture")
+				void video.offsetHeight
+				cl.add("takepicture")
+				video.play()
+				video.controls = false
+				console.log(video)
+				send_source(video, video.videoWidth, video.videoHeight)
+			}
+			
+			
+			document.addEventListener("visibilitychange", () => {
+				if (document.visibilityState != "visible") {
+					clear_camera()
+				}
+			})
+	
+	
+			// Set a default image
+	
+			const img = html`<img crossOrigin="anonymous">`
+	
+			img.onload = () => {
+			console.log("helloo")
+				send_source(img, img.width, img.height)
+			}
+			img.src = "$(default_url)"
+			console.log(img)
 		});
-	}
-	stop.onclick = () => {
-		clear_camera()
-	}
-	popout.onclick = () => {
-		span.classList.toggle("popped-out")
-	}
-
-	shutter.onclick = () => {
-		const cl = video.classList
-		cl.remove("takepicture")
-		void video.offsetHeight
-		cl.add("takepicture")
-		video.play()
-		video.controls = false
-		console.log(video)
-		send_source(video, video.videoWidth, video.videoHeight)
-	}
-	
-	
-	document.addEventListener("visibilitychange", () => {
-		if (document.visibilityState != "visible") {
-			clear_camera()
-		}
-	})
-
-
-	// Set a default image
-
-	const img = html`<img crossOrigin="anonymous">`
-
-	img.onload = () => {
-	console.log("helloo")
-		send_source(img, img.width, img.height)
-	}
-	img.src = "$(default_url)"
-	console.log(img)
-</script>
+	</script>
 </span>
 """ |> HTML
 end
